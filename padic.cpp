83fb8e87b2eb9fa9b1f84980fb331b55989a1d91
eclipse-consulting <short.gamma@icloud.com>
2024-11-24 11:37:40 -0500

    C++ wrapper

--- padic.cpp
+++ padic.cpp
@@ -1,13 +1,3 @@
-/*
-    Copyright (C) 2011 Sebastian Pancratz
-
-    This file is part of FLINT.
-
-    FLINT is free software: you can redistribute it and/or modify it under
-    the terms of the GNU Lesser General Public License (LGPL) as published
-    by the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.  See <https://www.gnu.org/licenses/>.
-*/
 
 /*
     Demo FLINT program to demonstrate some use of the padic module.
